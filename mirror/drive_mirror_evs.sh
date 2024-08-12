a0a426a18c5e49befea3224c5fa75db426854dd9
Mallory Row <mallory.row@noaa.gov>
2024-08-12 15:25:53 +0000

    Increase walltime

--- mirror/drive_mirror_evs.sh
+++ mirror/drive_mirror_evs.sh
@@ -19,4 +19,4 @@ COMPONENT=${1:-"component"}
 mkdir -p ${LOGSevs_util}
 
 # Submit to queue
-qsub -q "dev_transfer" -A "VERF-DEV" -S /bin/bash -N mirror_evs_${COMPONENT} -o ${LOGSevs_util}/log_mirror_evs_${COMPONENT}_run${now}.out -e ${LOGSevs_util}/log_mirror_evs_${COMPONENT}_run${now}.out -l walltime=02:00:00 -l select=1:ncpus=1 -l debug=true -v COMPONENT=${COMPONENT} ${HOMEevs_util}/mirror/mirror_evs.sh
+qsub -q "dev_transfer" -A "VERF-DEV" -S /bin/bash -N mirror_evs_${COMPONENT} -o ${LOGSevs_util}/log_mirror_evs_${COMPONENT}_run${now}.out -e ${LOGSevs_util}/log_mirror_evs_${COMPONENT}_run${now}.out -l walltime=06:00:00 -l select=1:ncpus=1 -l debug=true -v COMPONENT=${COMPONENT} ${HOMEevs_util}/mirror/mirror_evs.sh
