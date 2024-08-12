257a1b1153e4c4c1a91264796b613a8529b6ddc7
Mallory Row <mallory.row@noaa.gov>
2024-08-12 15:25:04 +0000

    Add run time to log name

--- mirror/drive_mirror_evs.sh
+++ mirror/drive_mirror_evs.sh
@@ -10,6 +10,8 @@ set -x
 HOMEevs_util=/lfs/h2/emc/vpppg/noscrub/${USER}/EVS-util
 LOGSevs_util=/lfs/h2/emc/ptmp/${USER}/evs_util_logs
 
+now=$(date '+%Y%m%d%H%M%S')
+
 # Get EVS COMPONENT
 COMPONENT=${1:-"component"}
 
@@ -17,4 +19,4 @@ COMPONENT=${1:-"component"}
 mkdir -p ${LOGSevs_util}
 
 # Submit to queue
-qsub -q "dev_transfer" -A "VERF-DEV" -S /bin/bash -N mirror_evs_${COMPONENT} -o ${LOGSevs_util}/log_mirror_evs_${COMPONENT}.out -e ${LOGSevs_util}/log_mirror_evs_${COMPONENT}.out -l walltime=02:00:00 -l select=1:ncpus=1 -l debug=true -v COMPONENT=${COMPONENT} ${HOMEevs_util}/mirror/mirror_evs.sh
+qsub -q "dev_transfer" -A "VERF-DEV" -S /bin/bash -N mirror_evs_${COMPONENT} -o ${LOGSevs_util}/log_mirror_evs_${COMPONENT}_run${now}.out -e ${LOGSevs_util}/log_mirror_evs_${COMPONENT}_run${now}.out -l walltime=02:00:00 -l select=1:ncpus=1 -l debug=true -v COMPONENT=${COMPONENT} ${HOMEevs_util}/mirror/mirror_evs.sh
