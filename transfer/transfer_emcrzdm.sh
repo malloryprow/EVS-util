21e848da7ac53f18580e236c3eebf1a8a7bf96b0
Mallory Row <mallory.row@noaa.gov>
2024-09-09 12:59:10 +0000

    Set new EMCRZDM transfer location and retention

--- transfer/transfer_emcrzdm.sh
+++ transfer/transfer_emcrzdm.sh
@@ -22,7 +22,7 @@ user_para=/lfs/h2/emc/ptmp/${USER}/evs/v2.0
 # RZDM Info
 webhost_id=${webhost_id:-$USER}
 webhost=emcrzdm.ncep.noaa.gov
-emcrzdm_dir=/home/people/emc/${webhost_id}/dev_tar_files
+emcrzdm_dir=/home/people/emc/www/htdocs/users/verification/${webhost_id}/dev_tar_files
 
 #### Transfer
 COMPONENT_RUN_VDATE_web_dir=${emcrzdm_dir}/${COMPONENT}/${RUN}.${VDATE}
@@ -36,7 +36,7 @@ echo "Transfering ${user_para}/plots/${COMPONENT}/${RUN}.${VDATE}/*.tar to ${COM
 rsync -ahr -P ${user_para}/plots/${COMPONENT}/${RUN}.${VDATE}/*.tar ${webhost_id}@${webhost}:${COMPONENT_RUN_VDATE_web_dir}/.
 
 #### Remove old directories
-rm_VDATE=$($NDATE -120 ${VDATE}00 |cut -c1-8)
+rm_VDATE=$($NDATE -72 ${VDATE}00 |cut -c1-8)
 COMPONENT_RUN_rm_VDATE_web_dir=${emcrzdm_dir}/${COMPONENT}/${RUN}.${rm_VDATE}
 echo "Removing ${COMPONENT_RUN_rm_VDATE_web_dir} on ${webhost}"
 ssh -q -l ${webhost_id} ${webhost} "rm -r ${COMPONENT_RUN_rm_VDATE_web_dir}"
